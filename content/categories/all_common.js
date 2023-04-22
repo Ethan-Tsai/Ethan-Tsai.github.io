aa8f1eb89757af8e58bde4d9e13cd74fcbd57415
Ethan-Tsai <ethan-tsai-0129@outlook.com>
2023-04-23 06:01:18 +0800

    color_end
    0423

--- content/categories/all_common.js
+++ content/categories/all_common.js
@@ -5,11 +5,13 @@ $(function() {
         if (scroll >= shrinkHeader) {
             $('.header').addClass('shrink');
             $('#logo_img').addClass('hide');
-
+            $('#title').addClass('change');
+            $('#h1').addClass('change');
         } else {
             $('.header').removeClass('shrink');
             $('#logo_img').removeClass('hide');
-
+            $('#title').removeClass('change');
+            $('#h1').removeClass('change');
         }
     });
 
