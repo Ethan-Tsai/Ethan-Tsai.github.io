f296c66a03489ff8580e23d481a4169284c5ee6e
Ethan-Tsai <ethan-tsai-0129@outlook.com>
2023-04-25 22:06:51 +0800

    progress_bar
    0425

--- content/categories/all_common.js
+++ content/categories/all_common.js
@@ -21,3 +21,12 @@ $(function() {
         return window.pageYOffset;
     }
 });
+
+window.onscroll = function() { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
